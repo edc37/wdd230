1d530008d011e11d9c61130843a1bae293defede
edc37 <grammar.nazi.410@gmail.com>
2023-05-13 23:49:54 -0500

    chamber of commerce and css

---
+++ chamber/scripts/pawneeJS.js
@@ -0,0 +1,40 @@
+let dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+let monthNames= [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+let d = new Date();
+let dayName = dayNames[d.getDay()];
+let monthName = monthNames[d.getMonth()];
+let year = d.getFullYear();
+let fullDate = (`${dayName}, ${d.getDate()} ${monthName} ${year}`)
+document.querySelector("#today").innerHTML = fullDate;
+
+let mod = document.lastModified;
+document.querySelector("#date-modified").innerHTML = mod;
+
+function toggleMenu () {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
---
+++ scripts/portal.js
@@ -0,0 +1,2 @@
+let mod = document.lastModified;
+document.querySelector("#date-modified").innerHTML = mod;
