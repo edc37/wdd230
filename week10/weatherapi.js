e8e3b7427df48021c01ccec1f2b9b29347d8915e
edc37 <grammar.nazi.410@gmail.com>
2023-06-20 21:52:55 -0500

    weather api

---
+++ week10/weatherapi.js
@@ -0,0 +1,33 @@
+const currentTemp = document.querySelector("#current_temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=32.7767&lon=-96.8088&units=imerpial&appid=c51d7a703983b1efa09d6ceb78c5ce37"
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // This is for testing the call
+            displayResults(data)
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
+
+apiFetch();
