d74e4a8da1e673472b9b08c437fb197d483a09a6
edc37 <grammar.nazi.410@gmail.com>
2023-06-21 22:15:16 -0500

    weather!

--- chamber/scripts/pawneeJS.js
+++ chamber/scripts/pawneeJS.js
@@ -48,7 +48,7 @@ if (dayNum == 1 || dayNum == 2) {
     alert("🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m." );
 }
 
-/* WINDCHILL */
+/* WINDCHILL 
 
 function doInputOutput() {
     let tempF = parseFloat(document.getElementById("temp").value);
@@ -59,7 +59,7 @@ function doInputOutput() {
 function windChill(tempF, speed) {
     let chill = 35.74 + .6215 * tempF - (35.75 * speed ** .16) + (.4275 * tempF * (speed ** .16));
     return chill.toFixed(2);
-}
+}*/
 
 /* Submit Timestamp */
 function timestamp() {
@@ -71,6 +71,8 @@ function timestamp() {
 const currentTemp = document.querySelector("#currentTemp");
 const weatherIcon = document.querySelector("#weatherIcon");
 const captionDesc = document.querySelector("figcaption");
+const windSpeed = document.querySelector("#windSpeed");
+const windChill = document.querySelector("#windChill");
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=39.1653&lon=-86.5264&units=imperial&appid=c51d7a703983b1efa09d6ceb78c5ce37";
 
@@ -91,12 +93,23 @@ async function apiFetch() {
 
 function displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-    
+    windSpeed.innerHTML = `<strong>${weatherData.wind.speed} mph</strong`;
+
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
 
+    const arr = desc.split("");
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    const desc2 = arr.join("");
+
     weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;
+    weatherIcon.setAttribute('alt', desc2);
+    captionDesc.textContent = desc2;
+
+    const chill = 35.74 + .6215 * weatherData.main.temp - (35.75 * weatherData.wind.speed ** .16) + (.4275 * weatherData.main.temp * (weatherData.wind.speed ** .16));
+
+    windChill.innerHTML = `<strong>${chill.toFixed(2)}</strong>`;
 }
 apiFetch();
