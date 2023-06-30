fd121ba5093edc67d344b07d9b38f0dcbc41e78c
edc37 <grammar.nazi.410@gmail.com>
2023-06-30 16:07:08 -0500

    directory fix, contact update, start spotlight

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,91 +1,26 @@
-let directory = `[
-    {
-        "name" : "Paunch Burger",
-        "address" :"2137 Peter St",
-        "phone" : "930-728-6242",
-        "website" : "https://parksandrecreation.fandom.com/wiki/Paunch_Burger?so=search",
-        "image" : "images/paunch_burger.webp",
-        "membership" : "Silver"
-    },
-    
-    {
-        "name" : "Sweetums Foundation",
-        "address" :"15722 Township Rd",
-        "phone" : "930-793-3886",
-        "website" : "https://parksandrecreation.fandom.com/wiki/Sweetums?so=search",
-        "image" : "images/sweetums.webp",
-        "membership" : "Gold"
-    },
-    
-    {
-        "name" : "Pawnee City Hall",
-        "address" :"1817 Union St",
-        "phone" : "930-729-6334",
-        "website" : "https://parksandrecreation.fandom.com/wiki/Pawnee_City_Hall?so=search",
-        "image" : "images/city_hall.webp",
-        "membership" : "Bronze"
-    },
-    
-    {
-        "name" : "Snakehole Lounge",
-        "address" :"26001 Burnham Ave",
-        "phone" : "930-762-5354",
-        "website" : "https://parksandrecreation.fandom.com/wiki/The_Snakehole_Lounge?so=search",
-        "image" : "images/snakehole_directory.webp",
-        "membership" : "Gold"
-    },
+/* JSON */
+/* const response = await fetch('json/chamberDirectory.json'); */
 
-    {
-        "name" : "Wamapoke Casino",
-        "address" :"2011 Casino Way",
-        "phone" : "930-926-7653",
-        "website" : "https://parksandrecreation.fandom.com/wiki/Wamapoke_Casino?so=search",
-        "image" : "images/wamapoke_casino.webp",
-        "membership" : ""
-    },
-
-    {
-        "name" : "Rent-A-Swag",
-        "address" :"12059 Ventura Plc",
-        "phone" : "930-289-7924",
-        "website" : "https://parksandrecreation.fandom.com/wiki/Rent-A-Swag?so=search",
-        "image" : "images/rent-a-swag.webp",
-        "membership" : "Bronze"
-    },
-
-    {
-        "name" : "Tom's Bistro",
-        "address" :"5936 Delancey Blvd ",
-        "phone" : "930-824-7876",
-        "website" : "https://parksandrecreation.fandom.com/wiki/Tom's_Bistro?so=search",
-        "image" : "images/toms_bistro.webp",
-        "membership" : "Silver"
-    },
-
-    {
-        "name" : "Entertainment 720",
-        "address" :"602 Trumball St",
-        "phone" : "930-720-7200",
-        "website" : "https://parksandrecreation.fandom.com/wiki/Entertainment_720?so=search",
-        "image" : "images/entertainment720",
-        "membership" : "Gold"
-    },
-
-    {
-        "name" : "Kernston's Rubber Nipples",
-        "address" :"1959 Porter Dr",
-        "phone" : "930-647-7537",
-        "website" : "https://parksandrecreation.fandom.com/wiki/Kernston%27s_Rubber_Nipples",
-        "image" : "images/kernstons_rubber_nipples.webp",
-        "membership" : "Bronze"
-    }
-]`
-
-const businesses = JSON.parse(directory)[0].name;
-businesses.forEach(business => {
-    const name = business.name;
-});
-
-document.querySelector("#output").innerHTML = burger;
-
-console.log(JSON.parse(directory)[0]);
+/*fetch('https://hp-api.onrender.com/api/characters')
+.then(res => {
+	return res.json();
+})
+.then(data => {;
+	data.forEach(character => {
+		const markup = `<li>${character.name}, ${character.house}, ${character.dateOfBirth}</li>`;
+	
+		document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
+	});
+})
+.catch(error => console.log(error)); */
+const link = 'scripts/json/directory.json';
+fetch("scripts/json/directory.json")
+.then(res => {
+    return res.json();
+})
+.then(data => {
+    data.forEach(business => {
+        const markup = `<h3>${business.name}</h3`;
+        document.querySelector("#directory").insertAdjacentHTML("beforeend", markup);
+    })
+})
--- chamber/scripts/pawneeJS.js
+++ chamber/scripts/pawneeJS.js
@@ -116,3 +116,44 @@ apiFetch();
 
 /* JSON */
 /* const response = await fetch('json/chamberDirectory.json'); */
+
+/*fetch('https://hp-api.onrender.com/api/characters')
+.then(res => {
+	return res.json();
+})
+.then(data => {;
+	data.forEach(character => {
+		const markup = `<li>${character.name}, ${character.house}, ${character.dateOfBirth}</li>`;
+	
+		document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
+	});
+})
+.catch(error => console.log(error)); 
+
+const link = 'scripts/json/directory.json';
+fetch("scripts/json/directory.json")
+.then(res => {
+    return res.json();
+})
+.then(data => {
+    data.forEach(business => {
+        const markup = `<h3>${business.name}</h3`;
+        document.querySelector("#directory").insertAdjacentHTML("beforeend", markup);
+    })
+})*/
+
+/*---SPOTLIGHTS---*/
+fetch("scripts/json/directory.json")
+.then(res => {
+    return res.json();
+})
+.then(data => {
+    data.forEach(business => {
+        const image = business.image;
+        const member = business.membership;
+
+        if (member = "Gold") {
+            document.querySelector("#spotlight1").value = image;
+        }
+    })
+})
