351d63b311bc87a6ce9fbfa31ac3c3031e26a2bc
edc37 <grammar.nazi.410@gmail.com>
2023-07-06 22:19:20 -0500

    bountiful

---
+++ bountiful/scripts/bountiful.js
@@ -0,0 +1,43 @@
+/* Display date */
+let dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+let monthNames= [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+let d = new Date();
+let dayName = dayNames[d.getDay()];
+let monthName = monthNames[d.getMonth()];
+let year = d.getFullYear();
+let fullDate = (`${dayName}, ${d.getDate()} ${monthName} ${year}`)
+document.querySelector("#today").innerHTML = fullDate;
+
+/* last modified */
+let mod = document.lastModified;
+document.querySelector("#date-modified").innerHTML = mod;
+
+/* hamburger */
+function toggleMenu () {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
